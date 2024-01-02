fb44d2528094a89ebd077f5702014ac56ba3a272
Arthur Gomes <ag0134486@gmail.com>
2024-01-02 14:33:36 -0300

    Classe usuario

---
+++ src/main/java/br/com/gomes/api/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.gomes.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
