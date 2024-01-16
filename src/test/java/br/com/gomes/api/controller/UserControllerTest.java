d1a9b76695ab2e1cf49563b29d208ae153ae5738
Arthur Gomes <ag0134486@gmail.com>
2024-01-16 20:58:34 -0300

    classe test para userController metodo fondById implementado

--- src/test/java/br/com/gomes/api/controller/UserControllerTest.java
+++ src/test/java/br/com/gomes/api/controller/UserControllerTest.java
@@ -10,7 +10,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,19 +29,35 @@ class UserControllerTest {
     private UserDTO userDTO;
 
     @InjectMocks
+    private UserController userController;
+    @Mock
     private ModelMapper mapper;
     @Mock
     private UserService userService;
-    @Mock
-    private UserController userController;
+
 
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
         startUser();
     }
+
+
     @Test
-    void findById() {
+    void whenFindByIdThenRetunrSuccess() {
+        when(userService.findById(anyLong())).thenReturn(user);
+        when(mapper.map(any(),any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = userController.findById(ID);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
